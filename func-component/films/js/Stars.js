8fac7e2e8d5ac5f47d674516291869b745b76f2b
serp-ya <sergei.kundryukov1989@gmail.com>
2017-12-05 18:06:18 +0300

    Сделал более подходящий key, исправил проверки

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -4,18 +4,21 @@ function Stars(props) {
   const currentRatingCount = parseInt(props.count);
   const validRatingCountConditions = [
     !!currentRatingCount,
-    currentRatingCount >= 1,
+    currentRatingCount > 0,
     currentRatingCount <= 5
   ];
+
+  if (!validRatingCountConditions.every(condition => condition === true)) {
+    return null;
+  }
 
   const starsList = [];
 
   for (let i = 0; i < currentRatingCount; i++) {
-    starsList.push((
+    starsList.push(
       <li key={i}><Star /></li>
-    ));
+    );
   }
 
-  return ((validRatingCountConditions.every(condition => condition === true)) ?
-    <ul className="card-body-stars u-clearfix">{starsList}</ul> : null);
+  return (<ul className="card-body-stars u-clearfix">{starsList}</ul>);
 }
